6c4886943c74e02d1c13c0099103319adfd00274
tNahoko <nahoko.toyota@gmail.com>
2021-02-24 19:01:14 +0900

    engine var connected to api

--- python_code/app.py
+++ python_code/app.py
@@ -1,6 +1,22 @@
+import os
+import pickle
 from flask import Flask
+from flask_restful import Resource, Api, reqparse, abort
+from flask_cors import CORS
+import numpy as np
+
+from recommendation import engine
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+@app.route('/wine', methods=['GET'])
+def wine():
+  print(engine.sorted_scores)
+  return {
+    "title": "Some wine",
+    "body": "<p>this is a wine</p>",
+    'wine_data': engine.sorted_scores
+  }
+
+if __name__ == '__main__':
+  app.run(debug=True)
